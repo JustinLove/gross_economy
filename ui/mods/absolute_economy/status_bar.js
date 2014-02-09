7b93d1164f60ae69bd857ff3a2b4d4d136789b16
Justin Love <git@JustinLove.name>
2014-02-09 11:01:25 -0600

    status bar model

--- ui/mods/absolute_economy/status_bar.js
+++ ui/mods/absolute_economy/status_bar.js
@@ -1,11 +1,30 @@
 (function() {
-    console.log('run', location)
-    loadTemplate = function ($element, url, model) {
+    var loadTemplate = function ($element, url, model) {
         $element.load(url, function () {
-            console.log("Loading html " + url, arguments);
+            console.log("Loading html " + url);
             ko.applyBindings(model, $element[0]);
         });
     };
 
-    loadTemplate($('.div_status_bar_cont'), 'coui://ui/mods/absolute_economy/status_bar.html', model);
+    var statusBarModel = {
+      currentMetal: model.currentMetal,
+      currentEnergy: model.currentEnergy,
+      maxMetal: model.maxMetal,
+      maxEnergy: model.maxEnergy,
+      metalGain: model.metalGain,
+      energyGain: model.energyGain,
+      metalLoss: model.metalLoss,
+      energyLoss: model.energyLoss,
+      metalDelta: model.metalDelta,
+      energyDelta: model.energyDelta,
+      metalNet: model.metalNet,
+      energyNet: model.energyNet,
+      metalNetString: model.metalNetString,
+      energyNetString: model.energyNetString,
+      metalFractionString: model.metalFractionString,
+      energyFractionString: model.energyFractionString,
+      showResources: model.showResources,
+    }
+
+    loadTemplate($('.div_status_bar_cont'), 'coui://ui/mods/absolute_economy/status_bar.html', statusBarModel);
 })()
