775cc69f15a30c8136b281bf78313063a6c563f3
Justin Love <git@JustinLove.name>
2014-02-19 09:01:00 -0600

    update for 61250

--- ui/mods/gross_economy/status_bar.js
+++ ui/mods/gross_economy/status_bar.js
@@ -149,7 +149,7 @@
 
   $.get('coui://ui/mods/gross_economy/status_bar_resource.html', function(html) {
     console.log("Gross Economy loaded HTML, modifing status bar");
-    loadTemplate($('.div_status_bar_cont .left_angle'), html, metal);
-    loadTemplate($('.div_status_bar_cont .right_flat'), html, energy);
+    loadTemplate($('.div_status_bar .left_angle'), html, metal);
+    loadTemplate($('.div_status_bar .right_flat'), html, energy);
   });
 })()
