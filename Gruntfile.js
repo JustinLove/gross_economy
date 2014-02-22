78d2eeef89fc00588a578c16fb94d1f188b11e7e
Justin Love <git@JustinLove.name>
2014-02-21 21:46:39 -0600

    copy files and fixup modinfo

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,36 @@ module.exports = function(grunt) {
 
           //name: 'lib/ext/almond',
           include: 'gross_economy/bootstrap',
-          out: 'dist/ui/mods/gross_economy/status_bar.js',
+          out: 'dist/ui/mods/gross_economy/bootstrap.js',
+        }
+      }
+    },
+    copy: {
+      simple: {
+        files: [
+          {
+            src: [
+              'LICENSE.txt',
+              'README.md',
+              'ui/mods/gross_economy/*.css'],
+            dest: 'dist/',
+          },
+        ],
+      },
+      modinfo: {
+        files: [
+          {
+            src: 'modinfo.pretty.json',
+            dest: 'dist/modinfo.json',
+          },
+        ],
+        options: {
+          process: function(content, srcpath) {
+            content = content.replace('Gross Economy', 'Gross Economy Test')
+            content = content.replace('"gross_economy"', '"gross_economy_test"')
+            content = content.replace('com.wondible.pa.gross_economy', 'com.wondible.pa.gross_economy_test')
+            return content
+          }
         }
       }
     },
@@ -21,8 +50,9 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['requirejs']);
+  grunt.registerTask('default', ['requirejs', 'copy']);
 
 };
