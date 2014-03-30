782870daef10657e50671de6df7a1ef780074e95
Justin Love <git@JustinLove.name>
2014-03-30 14:43:14 -0500

    looks like they removed prints from army event

--- ui/mods/gross_economy/bootstrap.js
+++ ui/mods/gross_economy/bootstrap.js
@@ -4,21 +4,4 @@ require.config({
     text: 'gross_economy/text',
   }
 })
-  // shutup
-  handlers.army = function (payload) {
-    model.currentEnergy(payload.energy.current);
-    model.maxEnergy(payload.energy.storage);
-    model.energyGain(payload.energy.production);
-    model.energyLoss(payload.energy.demand);
-
-    model.currentMetal(payload.metal.current);
-    model.maxMetal(payload.metal.storage);
-    model.metalGain(payload.metal.production);
-    model.metalLoss(payload.metal.demand);
-
-    model.hasFirstResourceUpdate(true);
-    //console.log('army message:');
-    //console.log(payload);
-  }
-
 require(['gross_economy/main'])
